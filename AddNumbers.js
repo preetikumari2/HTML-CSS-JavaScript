0da039cf5ae4a0f4aef73c8385b3e1bb383c8596
preetikumari2 <preethi.nisha1999@gmail.com>
2022-09-29 07:16:06 +0530

    Friendsbook commit

---
+++ AddNumbers.js
@@ -0,0 +1,9 @@
+let sum = 0
+function addAll(Number)
+{
+    for(let i = 0; i <= Number; i++){
+        sum = sum + i;
+    }
+}
+const value = addAll(10);
+console.log(value)
---
+++ Remove_threeChar.js
@@ -0,0 +1,7 @@
+function removeCharacter(string)
+{
+    string = string.slice(3)
+    return string 
+}
+const string = "Preethi"
+console.log(removeCharacter(string));
