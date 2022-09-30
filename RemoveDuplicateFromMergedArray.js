69abe7d8b4e937dffb5f513e47be3d7fbffd182a
preetikumari2 <preethi.nisha1999@gmail.com>
2022-09-30 10:37:26 +0530

    commit

--- RemoveDuplicateFromMergedArray.js
+++ RemoveDuplicateFromMergedArray.js
@@ -1,1 +1,8 @@
-uygf
+function removeDuplicate(array1, array2){
+    let array = [...array1, ...array2]
+    let newArray = [...new Set(array)]
+    console.log(newArray.sort())
+}
+const array1 = [1, 3, 4]
+const array2 = [2, 3, 5]
+removeDuplicate(array1, array2)
