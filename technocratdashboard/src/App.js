769d40fa0ae6354ab6fe8bfea828a4200ca33b2a
preetikumari2 <preethi.nisha1999@gmail.com>
2022-10-25 11:43:43 +0530

    commit

--- technocratdashboard/src/App.js
+++ technocratdashboard/src/App.js
@@ -1,30 +1,3 @@
-// // import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-// import './App.css';
-// import Sidebar from './Dashboard/Sidebar/Sidebar';
-// //import Login from './LoginForCEO/Login';
-// //import Home from './Dashboard/Home';
-// // import ClientsList from './Dashboard/ClientsList';
-// // import Protected from "./Dashboard/Protected";
-// // import Product from "./Dashboard/Product";
-// // import Sales from "./Dashboard/Sales";
-
-
-// function App() {
-
-//   return (
-//     <div className="App">
-//       <div className='AppClass'>
-//         <Sidebar />
-//         <div></div>
-//         <div></div>
-//       </div>
-//     {/* <Login/> */}
-//     {/* <Home /> */}
-//     </div>
-//   );
-// }
-
-
 import './App.css'
 import Login from './LoginForCEO/Login';
 import Home from './Dashboard/Home';
